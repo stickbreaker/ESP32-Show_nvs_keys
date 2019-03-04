a0d2893e23f1b01b2b4ce0f2a014c983937e6fc9
Chuck Todd <ctodd@cableone.net>
2019-03-04 12:19:43 -0700

    Non working initial for Spanning Blobs

--- Show_nvs_keys.ino
+++ Show_nvs_keys.ino
@@ -23,6 +23,7 @@ enum class ItemType : uint8_t {
     I64  = 0x18,
     SZ   = 0x21,
     BLOB = 0x41,
+	BLOB_SPAN = 0X42, //20190303
     ANY  = 0xff
 };
 
@@ -74,10 +75,16 @@ const uint32_t  INVALID       = 0;
 struct NVS_DATA{
   union{
     struct {
-      uint16_t size;
+      uint16_t size16;
       uint16_t rsv;
       uint32_t CRC;
       };
+	struct {
+		uint32_t size32;
+		uint8_t chunkCount;
+		uint8_t chunkStart;
+		uint16_t rsv2;
+	};
     struct {
       uint8_t data_u8;
       uint8_t f0[7];
@@ -115,7 +122,7 @@ struct NVS_ENTRY {
   uint8_t  Ns;         // Namespace ID
   uint8_t  Type;       // Type of value
   uint8_t  Span;       // Number of entries used for this item
-  uint8_t  Rvs;        // Reserved, should be 0xFF
+  uint8_t  ChunkIndex; // Index of this chunk if Type == 0x42(BLOB)
   uint32_t CRC;        // CRC
   char     Key[16];    // Key in Ascii
   NVS_DATA Data;       // Data in entry 
@@ -124,7 +131,8 @@ struct NVS_ENTRY {
 struct nvs_page {       // For nvs entries 1 page is 4096
   uint32_t  State;     
   uint32_t  Seqnr;
-  uint32_t  Unused[5];
+  uint8_t 	Version;   // 0xff=0, 0xFE = 1 ... 
+  uint8_t  Unused[19];
   uint32_t  CRC;
   uint8_t   Bitmap[32];
   NVS_ENTRY Entry[126];
@@ -143,7 +151,7 @@ struct NS_ARRAY{
   
 // Common data
 nvs_page buf;
-uint8_t * pageOrder=NULL; // current sequence for nvs pages pageOrder[0] is first page,
+uint32_t * pageOrder=NULL; // current sequence for nvs pages pageOrder[0] is first page,
   // usage is: offset = pageOrder[page]*sizeof(nvs_page);
 NS_ARRAY * NA=NULL;  
 
@@ -235,34 +243,23 @@ if(NA) free(NA);
 NA =(NS_ARRAY*)malloc(1);
 NA->count=0;
 if(pageOrder) free(pageOrder);
-uint8_t i = 0;
 uint16_t offset = 0;
 esp_err_t result = ESP_OK;
-i = nvs->size / sizeof(nvs_page);
-pageOrder = (uint8_t*)malloc(i);
-while(i>0) pageOrder[--i]=255;
+uint32_t pageCount = nvs->size / sizeof(nvs_page); // Calculate number of page control records
+pageOrder = (uint32_t*)malloc(pageCount*4);
+uint32_t i = 0;
+while(i<pageCount) pageOrder[i++]=0xFFFFFFFF;
 i=0;
 offset=0;
-while(i< (nvs->size / sizeof(nvs_page))){
+while(i< ( pageCount)){
   offset = i*sizeof(nvs_page);
   result = esp_partition_read( nvs, offset, &buf, sizeof(nvs_page));  // 
   if(result == ESP_OK){
     Serial.printf("page[%u].State=0x%lx ",i,buf.State);
     dispPageState(buf.State);
     if((buf.State == ACTIVE)||(buf.State==FULL)){
-      if(buf.Seqnr>((nvs->size / sizeof(nvs_page))-1)){
-        Serial.printf("Sequence value InValid: page[%u].Seqnr=%u ",i,buf.Seqnr);
-        }
-      else {
-        if(pageOrder[buf.Seqnr]!=255){
-          Serial.printf("Sequence error %u = %u, pageOrder[%u]=%u Already inuse!",i,buf.Seqnr,buf.Seqnr,pageOrder[
-           buf.Seqnr]);
-          }
-        else {
-          pageOrder[buf.Seqnr] = i;
-          Serial.printf(" Seqnr=%u ",buf.Seqnr);
-          }
-        }
+      pageOrder[i] = buf.Seqnr;
+      Serial.printf(" Seqnr=%u ",buf.Seqnr);
       }
     else Serial.printf(" Page[%u] not active, State = 0x%lx Seqnr=%u ",i,buf.State,buf.Seqnr);
     Serial.println("\n Entry bitmap\n"
@@ -274,12 +271,29 @@ while(i< (nvs->size / sizeof(nvs_page))){
   Serial.println();
   i++;
   }  
-
-/* manually create pageOrder because My ESP32 NVS is hosed
-pageOrder[1]=1; // jam  
-pageOrder[2]=3;
-pageOrder[3]=4;
-*/
+// re-sequence physical page to logical order
+uint32_t a = 0,j=0, k=0;
+uint16_t * sortOrder = (uint16_t*)malloc(pageCount*sizeof(uint16_t));
+while(k<pageCount){
+	i=0;
+	a=pageOrder[k];
+	j=0;
+	while(i<pageCount){
+		if(a>pageOrder[i]) j++;
+		i++;
+	}
+	sortOrder[k]=j;
+	k++;
+}
+k=0;
+while(k<pageCount){
+	if(pageOrder[k] != 0xFFFFFFFF) {
+		pageOrder[k]=sortOrder[k];
+	}
+	k++;
+}
+free(sortOrder);
+  
 Serial.printf("\nLogical Page to Physical Sector\n");
 for(i=0;i<(nvs->size / sizeof(nvs_page));i++){
   Serial.printf("Page %d -> %u\n",i,pageOrder[i]);
@@ -287,7 +301,7 @@ for(i=0;i<(nvs->size / sizeof(nvs_page));i++){
 // find namespaces
 i=0;
 while(i<(nvs->size / sizeof(nvs_page))){
-  if(pageOrder[i] != 255) {
+  if(pageOrder[i] != 0xFFFFFFFF) {
     offset = pageOrder[i] * sizeof(nvs_page);
     result = esp_partition_read( nvs, offset, &buf, sizeof(nvs_page));  // 
     if(result == ESP_OK){
@@ -373,8 +387,45 @@ switch(nvs->Type){
     desc=(char*)malloc(descSize);
     sprintf(desc,"size=%04x\n%s",nvs->Data.size,(char*)&buf.Entry[index+1]);
     break;
-  case 0x41: // BLOB
+  case 0x41: // BLOB  V.0
     sprintf(dtype,"BLOB");
+    
+    descSize = (nvs->Data.size/16);
+    if((nvs->Data.size%16)!=0) descSize++;
+    descSize = (descSize *(2+16*4))+1+11;
+    desc = (char*)malloc(descSize);
+    memset(desc,32,descSize);
+    desc[descSize-1] = '\0';
+ //   Serial.printf("Blob size=%d calc=%d\n",nvs->Data.size,descSize);
+    d1 = (char*)&buf.Entry[index+1];
+    i=sprintf(desc,"size=%04x\n",nvs->Data.size);
+    j=0;
+    n=0;
+    while(j< nvs->Data.size){
+      desc[i+((16-n)*3)+n+1] = (d1[j]>31)&&(d1[j]<128)?d1[j]:'.';
+      i+= sprintf((char*)&desc[i],"%02x ",d1[j]);
+      n++;
+      j++;
+      if(n>15){
+        desc[i]=' ';
+        desc[i+17]='\n';
+        i=i+18;
+        desc[i] = '\0';
+        n=0;
+        }
+      }
+    if(n>0){// partial line
+      desc[i]=' ';
+      i = i+17+(16-n)*3;
+      desc[i] = '\0';
+      }   
+    else { // complete line
+      desc[i-1] = '\0';
+      }    
+ break;
+  case 0x42: // BLOB  V.1 // spanning multiple logical pages
+	// ++++++++ this code does not work 04MAR2019
+	sprintf(dtype,"BLv1");
     
     descSize = (nvs->Data.size/16);
     if((nvs->Data.size%16)!=0) descSize++;
